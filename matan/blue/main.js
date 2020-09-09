e78e0790f51a1efc5fe03d094d924d78498c9515
Oded Network <obadt@infinidat.com>
2020-09-09 15:59:48 +0300

    Fixed mobile (4)

--- matan/blue/main.js
+++ matan/blue/main.js
@@ -1,58 +1,7 @@
 var state = {
     on: false,
     points: [],
-    dirty: false
-}
-function turn_on(event) {
-    state.on = true;
-    state.points.push([event.offsetX, event.offsetY])
-}
-
-function render() {
-    if (!state.dirty) {
-        return;
-    }
-    console.log(state)
-    var ctx = state.context;
-    ctx.clearRect(0, 0, state.W, state.H);
-    ctx.strokeStyle = 'blue';
-    ctx.fillStyle ='blue';
-    ctx.lineWidth = 10;
-    ctx.lineJoin = 'round';
-    var half = state.unit / 2;
-    for (var ang = 0; ang < 360; ang += 30) {
-        var transform = function(p) {
-            return shift([half, half], rotate(ang, shift([-half, -half], p)))
-        }
-        var points = state.points;
-        for (i = 0; i < state.points.length; ++i) {
-            var p = transform([points[i][0], points[i][1]])
-            if (i == 0) {
-                ctx.beginPath()
-                ctx.moveTo(p[0], p[1])
-            } else {
-                ctx.lineTo(p[0], p[1]);
-            }
-        }
-        ctx.closePath()
-        ctx.fill("nonzero");
-        ctx.stroke();
-    }
-    ctx.fillStyle ='violet';
-    ctx.beginPath();
-    ctx.arc(half, half, 2, 0, 2 * Math.PI);
-    ctx.fill();
-    state.dirty = false;
-}
-
-function loop() {
-    render();
-    window.requestAnimationFrame(loop);
-
-}
-function turn_off() {
-    state.on = false;
-    state.points = [];
+    dirty: true
 }
 function cmult(p1, p2) {
     return [p1[0]*p2[0] - p1[1]*p2[1], p1[0]*p2[1] + p1[1]*p2[0]]
@@ -67,24 +16,87 @@ function shift(offset, p) {
 function scale(u, p) {
     return [u*p[0], u*p[1]];
 }
+
+function render() {
+    if (!state.dirty) {
+        return;
+    }
+
+    var ctx = state.context;
+    ctx.clearRect(0, 0, state.W, state.H);
+    ctx.strokeStyle = 'red';
+    ctx.fillStyle ='blue';
+    ctx.lineWidth = 10;
+    ctx.lineJoin = 'round';
+
+    var half = state.unit / 2;
+    for (var ang = 0; ang < 360; ang += 360/7) {
+        for (var m = -1; m < 2; m +=2) {
+            var transform = function(p) {
+                var r = shift([half, half], rotate(ang, shift([-half, -half], p)));
+                if (m == -1) {
+                    return [r[0], state.unit-r[1]];
+                } else {
+                    return r;
+                }
+            }
+
+            var points = state.points;
+            for (i = 0; i < state.points.length; ++i) {
+                var p = transform([points[i][0], points[i][1]])
+                if (i == 0) {
+                    ctx.beginPath();
+                    ctx.moveTo(p[0], p[1])
+                } else {
+                    ctx.lineTo(p[0], p[1]);
+                }
+            }
+            ctx.fill("nonzero");
+
+        }
+    }
+
+    // ctx.beginPath();
+    // ctx.arc(half, half, 2, 0, 2 * Math.PI);
+    // ctx.closePath();
+
+    // ctx.fill();
+
+    state.dirty = false;
+}
+function loop() {
+    render();
+    window.requestAnimationFrame(loop);
+}
+
+function turn_on(event) {
+    state.on = true;
+    state.dirty = true;
+    state.points.push([event.offsetX, event.offsetY])
+}
+function turn_off() {
+    state.on = false;
+    state.points = [];
+}
 function ontouch(event) {
+  event.stopPropagation();
+  event.preventDefault();
   state.on = false;
-  state.dirty = true;
 
   var touches = event.changedTouches;
-
   for (var i = 0; i < touches.length; i++) {
-        state.points.push([touches[i].clientX, touches[i].clientY])
-    }
+    state.points.push([touches[i].clientX, touches[i].clientY])
+  }
+  state.dirty = true;
 }
 function onmove(event) {
     if (state.on) {
         state.points.push([event.offsetX, event.offsetY])
         state.dirty = true;
     }
+
 }
 function resize() {
-    console.log('RZ');
   canvas_element = $('.canvas')[0];
   state.W = window.innerWidth;
   state.H = window.innerHeight;
@@ -106,9 +118,9 @@ function ignite() {
     canvas_element.addEventListener('mouseleave', turn_off)
     canvas_element.addEventListener('mouseout', turn_off)
     canvas_element.addEventListener('mousemove', onmove)
-    canvas_element.addEventListener("touchstart", ontouch);
-    canvas_element.addEventListener("touchend", turn_off);
-    canvas_element.addEventListener("touchmove", ontouch);
-      window.addEventListener('resize', resize)
+    canvas_element.addEventListener("touchstart", ontouch, false);
+    canvas_element.addEventListener("touchend", turn_off, false);
+    canvas_element.addEventListener("touchmove", ontouch, false);
+    window.addEventListener('resize', resize)
     loop();
 }
