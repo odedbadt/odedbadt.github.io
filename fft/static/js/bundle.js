a7cec76f2591cb02a957862b489a210d4e888d62
Oded Badt <odedbadt@gmail.com>
2025-01-04 23:51:08 +0200

    Fidxed interpolation, add log, added margins, shift input signal

--- fft/static/js/bundle.js
+++ fft/static/js/bundle.js
@@ -58860,11 +58860,33 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var mathjs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! mathjs */ "./node_modules/mathjs/lib/esm/entry/pureFunctionsAny.generated.js");
 
 
+function fft_shift(array) {
+    const n = array.length;
+    const halfN = Math.floor(n / 2);
+
+    // Create a new array for the result
+    const shifted = new Float64Array(n);
+
+    if (n % 2 === 0) {
+        // For even-length arrays
+        shifted.set(array.subarray(halfN), 0); // Second half to the front
+        shifted.set(array.subarray(0, halfN), halfN); // First half to the back
+    } else {
+        // For odd-length arrays
+        shifted.set(array.subarray(halfN + 1), 0); // Second half excluding the middle to the front
+        shifted.set(array.subarray(0, halfN + 1), n - halfN - 1); // First half + middle to the back
+    }
+
+    return shifted;
+}
+
 class App {
     constructor() {
         this.dpr = window.devicePixelRatio;
         this.w = document.getElementById('sketcher').width * this.dpr;
-        this.signal = new Float32Array(this.w)
+        this.signal = new Float64Array(this.w)
+        this.mouse_signal = new Float64Array(this.w)
+        this.mouse_signal.fill(null, 0, this.w);
 
     }
     init() {
@@ -58874,40 +58896,52 @@ class App {
         this._prev_y = null
     
     }
+    fft_shift(x) {
+        if (x > this.w/2) {
+            return x - this.w/2
+        }
+        return x + this.w/2 
+    }
+    sym_fft_shift(x) {
+        return this.fft_shift(x)
+    }
     draw_fft() {
-                // Arrays to hold the real and imaginary outputs
-        const draw_signal = (context, signal, h, shift, scale) => {
+        const draw_signal = (context, signal, h, shift, scale, log) => {
             context.fillStyle="white"
             context.beginPath()
             context.fillRect(0,0,h,h)
             context.fill()
-            const shifting_function = (j) => j < signal.length/2 ? j + signal.length/2 : j - signal.length/2
-            const x_transfrmation = shift? shifting_function :(x)=> x
-
-            context.moveTo(0, signal[x_transfrmation(0)]*scale+h/2)
+            const x_transfrmation = shift ? this.fft_shift.bind(this) : (x) => x
+            const y_transformation = log ? (y) => -Math.sign(y)*Math.log(Math.abs(y*scale))+h/2 : 
+                (y) => -y*scale+h/2
+            context.moveTo(0, y_transformation(signal[x_transfrmation(0)]))
             context.strokeStyle='rgb(150,40,40)';
             for (let y = Math.floor(-h/scale); y < h/scale; ++y) {
                 context.beginPath()
-                context.moveTo(0,y*scale+h/2)
-                context.lineTo(h,y*scale+h/2)
+                context.moveTo(0,y_transformation(y)*scale+h/2)
+                context.lineTo(h,y_transformation(y)*scale+h/2)
                 context.stroke();
             }
             context.beginPath()
             context.strokeStyle='grey';
 
-            for (let j = 0; j < signal.length; ++j) {
+            const shifted0 = signal[this.fft_shift(0)]
+            context.moveTo(0, y_transformation(shift  ? shifted0 : signal[0]))
+            for (let j = 0; j < h; j++) {
 
-                const shifted = j < signal.length/2 ? j + signal.length/2 : j - signal.length/2
-                const v = signal[shift ? shifted : j];
-                const scaled = -v*scale+h/2
-                context.lineTo(j, scaled)
+                const shifted = this.sym_fft_shift(j)
+                const v = y_transformation(signal[shift ? shifted : j]);
+                
+                context.lineTo(j, v)
 
             }
             context.stroke()
+            context.fillStyle='grey';
+
         }
         const l = this.signal.length
-        const real = new Float32Array(l);
-        const imag = new Float32Array(l);
+        const real = new Float64Array(l);
+        const imag = new Float64Array(l);
         const real_canvas = document.getElementById('real')
         const real_context = real_canvas.getContext('2d', {'willreadFrequently': true})
         const imag_canvas = document.getElementById('imag')
@@ -58919,23 +58953,28 @@ class App {
         const arg_canvas = document.getElementById('arg');
         const arg_context = arg_canvas.getContext('2d', {'willreadFrequently': true})
 
+
         // const F = FFT;
         // for (let j = 0; j < l; ++j) {
         //     this.signal[j] = Math.sin(j/10); 
         // }
         // const fft = new FFT(l);
         // Perform the FFT
-        this.fft_result = (0,mathjs__WEBPACK_IMPORTED_MODULE_1__.fft)(Array.from(this.signal));
+        this.fft_result = (0,mathjs__WEBPACK_IMPORTED_MODULE_1__.fft)(Array.from(fft_shift(this.signal)));
         this.real_output =this.fft_result.map(complex => complex.re);
         this.imag_output =this.fft_result.map(complex => complex.im);
         this.abs_output = this.fft_result.map(complex => complex.abs());
         this.arg_output = this.fft_result.map(complex => complex.arg());
         const h = this.signal.length
-        draw_signal(real_context, this.real_output, real_canvas.height, true , (h*.25));
+        draw_signal(real_context, this.real_output, real_canvas.height, true , (h*.25), true);
         draw_signal(imag_context, this.imag_output, imag_canvas.height, true , (h*.25));
-        draw_signal(abs_context, this.abs_output, real_canvas.height, true , (h*.1));
+        draw_signal(abs_context, this.abs_output, real_canvas.height, true , (h*.1), true);
         draw_signal(arg_context, this.arg_output, imag_canvas.height, true , (h*.25/3.11));
         draw_signal(signal_context, this.signal, signal_canvas.height, false, (h*2));
+        const scale = h*2
+        const shift = false;
+
+
 
     }
     init_main_loop() {
@@ -58964,6 +59003,7 @@ class App {
             this._prev_y = y;
             const value = -(y - h / 2)/(h*2);
             this.signal[x] = value;
+            this.mouse_signal[x] = value;
         });
         sketcher_canvas.addEventListener('mousemove', (event) => {
             if (1 & event.buttons) {
@@ -58972,19 +59012,16 @@ class App {
                 if (x<0) {
                     return;
                 }                
+                const value = -(y - h / 2)/(h*2);
                 if (this._prev_x != null) {
                     const sgn = Math.sign(x - this._prev_x)
                     for (let j = 0; j < Math.abs(x - this._prev_x); ++j) {
                         const interpolation_x = this._prev_x+j*sgn;
-
-                         const value = -(y - h / 2)/(h*2);
-                         const prev_value = -(this._prev_y - h / 2)/(h*2);
-                        // const ratio = j / Math.abs(x - this._prev_x);
-                        // const interpolated_value = value_in_prev_x*ratio + value*(1 - ratio);
-                        // const prev_value_in_interpolation_x = this.signal[x+j*sgn]
-                            
-                        this.signal[interpolation_x] = (value*x + this._prev_x*prev_value)/
-                                        (x + this._prev_x);
+                        const ratio = j/Math.abs(x - this._prev_x);
+                        const prev_value = -(this._prev_y - h / 2)/(h*2);
+                        const interpolated_value = value + 
+                        (value - prev_value)*ratio
+                        this.signal[interpolation_x] = interpolated_value;
 
                     }
                     
@@ -58992,7 +59029,8 @@ class App {
                 }
                 this._prev_x = x;
                 this._prev_y = y;
-                this.signal[x] = -(y - h/2)/(h*2)
+                this.signal[x] = value;
+                this.mouse_signal[x] = value;
 
             }
 
